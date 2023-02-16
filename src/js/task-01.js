5d5f926cf282f53f9d73fc4eb6ccf6367e9217ba
Andrii-Sh <114801650+Andrii-Sh@users.noreply.github.com>
2023-02-16 23:33:11 +0200

    1st task comleted

---
+++ src/js/task-01.js
@@ -0,0 +1,23 @@
+const categories = document.querySelectorAll('.item');
+console.log("Number of categories:", categories.length);
+
+const list = document.querySelector('ul#categories');
+
+const firstCategoryTitle = list.firstElementChild.querySelector('h2');
+const firstCategoryElements = list.firstElementChild.querySelectorAll('li');
+console.log("Category:", firstCategoryTitle.textContent);
+console.log("Elements:", firstCategoryElements.length);
+
+const secondCategoryTitle = list.children[1].querySelector('h2');
+const secondCategoryElements = list.children[1].querySelectorAll('li');
+console.log("Category:", secondCategoryTitle.textContent);
+console.log("Elements:", secondCategoryElements.length);
+
+const lastCategoryTitle = list.lastElementChild.querySelector('h2');
+
+const lastCategoryElements = list.lastElementChild.querySelectorAll('li');
+console.log("Category:", lastCategoryTitle.textContent);
+console.log("Elements:", lastCategoryElements.length);
+
+
+
