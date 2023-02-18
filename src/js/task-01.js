6c2b82ed90e1e87992882862224f92ea7c53f8f4
Andrii-Sh <114801650+Andrii-Sh@users.noreply.github.com>
2023-02-18 16:36:50 +0200

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,22 +1,10 @@
-const categories = document.querySelectorAll('.item');
-console.log("Number of categories:", categories.length);
+const categoriesEl = document.querySelectorAll('.item');
+console.log("Number of categories:", categoriesEl.length);
 
-const list = document.querySelector('ul#categories');
+categoriesEl.forEach(element => {
+    const categoryTitleEl = element.querySelector('h2');
+    console.log("Category:", categoryTitleEl.textContent);
 
-const firstCategoryTitle = list.firstElementChild.querySelector('h2');
-const firstCategoryElements = list.firstElementChild.querySelectorAll('li');
-console.log("Category:", firstCategoryTitle.textContent);
-console.log("Elements:", firstCategoryElements.length);
-
-const secondCategoryTitle = list.children[1].querySelector('h2');
-const secondCategoryElements = list.children[1].querySelectorAll('li');
-console.log("Category:", secondCategoryTitle.textContent);
-console.log("Elements:", secondCategoryElements.length);
-
-const lastCategoryTitle = list.lastElementChild.querySelector('h2');
-const lastCategoryElements = list.lastElementChild.querySelectorAll('li');
-console.log("Category:", lastCategoryTitle.textContent);
-console.log("Elements:", lastCategoryElements.length);
-
-
-
+    const categoryElementsEl = element.querySelectorAll('li');
+    console.log("Elements:", categoryElementsEl.length);
+});
