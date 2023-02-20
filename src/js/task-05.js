a8414ee710bc9b2f1c0dcade93b01f2fafa09306
Andrii-Sh <114801650+Andrii-Sh@users.noreply.github.com>
2023-02-20 22:53:11 +0200

    task-06 completed

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -2,7 +2,7 @@ const form = document.querySelector('#name-input');
 const message = document.querySelector('#name-output');
 
 form.addEventListener("input", (event) =>
-    event.currentTarget.value === ""
+    event.currentTarget.value.trim() === ""
         ? message.textContent = "Anonymous"
         : message.textContent = event.currentTarget.value
     );
---
+++ src/js/task-06.js
@@ -0,0 +1,16 @@
+const textInput = document.querySelector('#validation-input');
+
+const REQUIRED_LENGTH = Number(textInput.getAttribute('data-length'));
+
+textInput.addEventListener("blur", (event) => { 
+
+    const inputValue = event.currentTarget.value.trim();
+
+    if (inputValue.length === REQUIRED_LENGTH) {
+        textInput.classList.add("valid");
+    } else {
+        textInput.classList.add("invalid");
+    }    
+
+});
+
