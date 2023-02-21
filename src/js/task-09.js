64a69e5124418244718ae761dd60055140e57a60
Andrii-Sh <114801650+Andrii-Sh@users.noreply.github.com>
2023-02-21 12:38:10 +0200

    task-09 completed

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const currentColorName = document.querySelector('span.color');
+const changeColorBtn = document.querySelector('button.change-color');
+
+changeColorBtn.addEventListener("click", changeHexColor);
+
+function changeHexColor() { 
+  const randomColor = getRandomHexColor();
+  currentColorName.textContent = randomColor;
+  document.body.style.backgroundColor = randomColor;
+}
