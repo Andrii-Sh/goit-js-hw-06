6f0d29b76a41242e122948fddbbc14b5a3b479a9
Andrii-Sh <114801650+Andrii-Sh@users.noreply.github.com>
2023-02-21 11:49:30 +0200

    task-08 completed

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) { 
+    event.preventDefault();
+
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Please fill in all the fields!");
+    } 
+        
+    const userCredentials = {
+        email: email.value,
+        password: password.value
+    };
+
+    console.log(userCredentials);
+
+    event.currentTarget.reset();    
+}
